0530e6e38cf93e81c458070ccce8eb269493223f
Jaakko-Heikki Heusala <jheusala@iki.fi>
2011-09-09 16:51:40 +0300

    Added transaction middlewares

--- test/test-pg.js
+++ test/test-pg.js
@@ -298,6 +298,103 @@ module.exports = testCase({
 			
 			test.done();
 		});
+	},
+	/* Test transaction middlewares */
+	transaction_inserts_successful: function(test){
+		var mytestcase = this;
+		test.expect(24);
+		
+		var insert = mytestcase.sql.group(
+			mytestcase.sql.begin(),
+			mytestcase.sql.query('INSERT INTO '+mytestcase.table+" (title, text, created) VALUES ('Hello World', 'Test article #1', '2011-06-08 12:10:00')"),
+			mytestcase.sql.query('INSERT INTO '+mytestcase.table+" (title, text, created) VALUES ('Second title', 'Test article #2', '2011-07-15 09:34:39')"),
+			mytestcase.sql.query('INSERT INTO '+mytestcase.table+" (title, text, created) VALUES ('Third title', 'Test article #3', '2011-09-07 20:00:00')"),
+			mytestcase.sql.commit()
+		);
+		test.ok(insert, "Failed to create callback");
+		test.strictEqual(typeof insert, 'function', "Failed to create callback");
+		insert(function(err) {
+			if(err) {
+				mytestcase.sql.rollback();
+				console.log('Error: ' + err);
+			}
+			
+			var select = mytestcase.sql.query('SELECT * FROM '+mytestcase.table)
+			select(function(err, state) {
+				test.ok(!err, "Error: " + err);
+				test.ok(state, "state");
+
+				test.ok(state._rows, "state._rows");
+				test.ok(state._results, "state._results");
+				test.strictEqual( state._rows.length, 3, "state._rows.length, 3");
+				test.strictEqual( state._results.length, 1, "state._results.length, 1");
+
+				test.strictEqual( state._results[0].length, 3, "state._results[0].length, 3");
+				test.strictEqual( sys.inspect(state._rows[0]), sys.inspect(state._results[0][0]), "Article #1 failed");
+				test.strictEqual( sys.inspect(state._rows[1]), sys.inspect(state._results[0][1]), "Article #2 failed");
+				test.strictEqual( sys.inspect(state._rows[2]), sys.inspect(state._results[0][2]), "Article #3 failed");
+				
+				test.strictEqual( state._rows[0].id, 1, "state._rows[0].id, 1");
+				test.strictEqual( state._rows[0].title, 'Hello World');
+				test.strictEqual( state._rows[0].text, 'Test article #1');
+				test.strictEqual( sys.inspect(state._rows[0].created), 'Wed, 08 Jun 2011 09:10:00 GMT');
+				
+				test.strictEqual( state._rows[1].id, 2, "state._rows[1].id, 2");
+				test.strictEqual( state._rows[1].title, 'Second title');
+				test.strictEqual( state._rows[1].text, 'Test article #2');
+				test.strictEqual( sys.inspect(state._rows[1].created), 'Fri, 15 Jul 2011 06:34:39 GMT');
+				
+				test.strictEqual( state._rows[2].id, 3, "state._rows[2].id, 3");
+				test.strictEqual( state._rows[2].title, 'Third title');
+				test.strictEqual( state._rows[2].text, 'Test article #3');
+				test.strictEqual( sys.inspect(state._rows[2].created), 'Wed, 07 Sep 2011 17:00:00 GMT');
+			
+				test.done();
+
+			});
+			
+		});
+	},
+	/* Test transaction middlewares */
+	transaction_inserts_failing: function(test){
+		var mytestcase = this;
+		test.expect(13);
+		
+		var insert = mytestcase.sql.group(
+			mytestcase.sql.begin(),
+			mytestcase.sql.query('INSERT INTO '+mytestcase.table+" (title, text, created) VALUES ('Hello World', 'Test article #1', '2011-06-08 12:10:00')"),
+			mytestcase.sql.query('INSERT INTO '+mytestcase.table+" (title, text, created) VALUES ('Second title', 'Test article #2', '2011-07-15 09:34:39')"),
+			mytestcase.sql.query('INSERT INTO '+mytestcase.table+" (title, text, created) VALUES ('Third title', 'Test article #3', '2011-09-07 20:00:00')"),
+			mytestcase.sql.rollback()
+		);
+		test.ok(insert, "Failed to create callback");
+		test.strictEqual(typeof insert, 'function', "Failed to create callback");
+		insert(function(err) {
+			if(err) {
+				mytestcase.sql.rollback();
+			}
+			test.ifError(err);
+			
+			var select = mytestcase.sql.query('SELECT * FROM '+mytestcase.table)
+			select(function(err, state) {
+				test.ifError(err);
+				test.ok(state, "state");
+				
+				test.ok(state._rows, "state._rows");
+				test.ok(state._results, "state._results");
+				test.strictEqual( state._rows.length, 0);
+				test.strictEqual( state._results.length, 1);
+				test.strictEqual( state._results[0].length, 0);
+				
+				test.strictEqual(state._rows[0], undefined, "state._rows[0]");
+				test.strictEqual(state._rows[1], undefined, "state._rows[1]");
+				test.strictEqual(state._rows[2], undefined, "state._rows[2]");
+				
+				test.done();
+
+			});
+			
+		});
 	}
 });
 
