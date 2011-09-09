69a3a8e5ea1ccb73ee1ef8bf4d46e8707fe62f73
Jaakko-Heikki Heusala <jheusala@iki.fi>
2011-09-09 08:52:57 +0300

    Simplified interface for modules

--- test/test-mysql.js
+++ test/test-mysql.js
@@ -26,6 +26,7 @@ module.exports = testCase({
 			callback(err);
 		});
 	},
+	/* Test for backend interface */
 	backend: function(test){
 		var mytestcase = this;
 		var backend = mytestcase.sql.backend;
@@ -38,7 +39,8 @@ module.exports = testCase({
 		test.strictEqual(typeof backend.disconnect, 'function', "Missing .disconnect");
 		test.done();
 	},
-	query_insert: function(test){
+	/* Test for INSERT with user defined callback function */
+	query_insert_with_callback: function(test){
 		var mytestcase = this;
 		test.expect(5);
 		var cb = mytestcase.sql.query('INSERT INTO '+mytestcase.table+' (title, text, created) VALUES (:title, :text, :created)');
@@ -51,7 +53,8 @@ module.exports = testCase({
 			test.done();
 		});
 	},
-	query_select: function(test){
+	/* Test for SELECT with empty options and user defined callback function */
+	query_select_with_empty_options_callback: function(test){
 		var mytestcase = this;
 		test.expect(24);
 		var cb = mytestcase.sql.group(
@@ -93,7 +96,51 @@ module.exports = testCase({
 			test.done();
 		});
 	},
-	query_select_single: function(test){
+	/* Test for SELECT without options and with user defined callback function */
+	query_select_without_options_with_callback: function(test){
+		var mytestcase = this;
+		test.expect(24);
+		var cb = mytestcase.sql.group(
+			mytestcase.sql.query('INSERT INTO '+mytestcase.table+" (title, text, created) "+
+				"VALUES ('Hello World', 'Test article #1', '2011-06-08 12:10:00'), "+
+				"('Second title', 'Test article #2', '2011-07-15 09:34:39'), "+
+				"('Third title', 'Test article #3', '2011-09-07 20:00:00')"),
+			mytestcase.sql.query('SELECT * FROM '+mytestcase.table)
+		);
+		test.ok(cb, "Failed to create callback");
+		test.strictEqual(typeof cb, 'function', "Failed to create callback");
+		cb(function(err, state) {
+			test.ok(!err, "Error: " + err);
+			test.ok(state);
+			test.ok(state._rows);
+			test.ok(state._results);
+			test.strictEqual( state._rows.length, 3);
+			test.strictEqual( state._results.length, 1);
+			test.strictEqual( state._results[0].length, 3);
+			test.strictEqual( sys.inspect(state._rows[0]), sys.inspect(state._results[0][0]), "Article #1 failed");
+			test.strictEqual( sys.inspect(state._rows[1]), sys.inspect(state._results[0][1]), "Article #2 failed");
+			test.strictEqual( sys.inspect(state._rows[2]), sys.inspect(state._results[0][2]), "Article #3 failed");
+			
+			test.strictEqual( state._rows[0].id, 1);
+			test.strictEqual( state._rows[0].title, 'Hello World');
+			test.strictEqual( state._rows[0].text, 'Test article #1');
+			test.strictEqual( sys.inspect(state._rows[0].created), 'Wed, 08 Jun 2011 09:10:00 GMT');
+			
+			test.strictEqual( state._rows[1].id, 2);
+			test.strictEqual( state._rows[1].title, 'Second title');
+			test.strictEqual( state._rows[1].text, 'Test article #2');
+			test.strictEqual( sys.inspect(state._rows[1].created), 'Fri, 15 Jul 2011 06:34:39 GMT');
+			
+			test.strictEqual( state._rows[2].id, 3);
+			test.strictEqual( state._rows[2].title, 'Third title');
+			test.strictEqual( state._rows[2].text, 'Test article #3');
+			test.strictEqual( sys.inspect(state._rows[2].created), 'Wed, 07 Sep 2011 17:00:00 GMT');
+			
+			test.done();
+		});
+	},
+	/* Test for SELECT + LIMIT with selected options and user defined callback function */
+	query_select_single_with_options_callback: function(test){
 		var mytestcase = this;
 		test.expect(20);
 		var cb = mytestcase.sql.group(
@@ -130,6 +177,7 @@ module.exports = testCase({
 			test.done();
 		});
 	},
+	/* Test for DELETE with selection (options) and user defined callback function */
 	query_delete: function(test){
 		var mytestcase = this;
 		test.expect(19);
@@ -167,6 +215,7 @@ module.exports = testCase({
 			test.done();
 		});
 	},
+	/* Test for UPDATE with options and user defined callback function */
 	query_update: function(test){
 		var mytestcase = this;
 		test.expect(24);
@@ -209,6 +258,92 @@ module.exports = testCase({
 			
 			test.done();
 		});
+	},
+	/* Test for DELETE with selection (options) but no user defined callback */
+	query_delete_with_options_without_callback: function(test){
+		var mytestcase = this;
+		test.expect(21);
+		var del = mytestcase.sql.group(
+			mytestcase.sql.query('INSERT INTO '+mytestcase.table+" (title, text, created) "+
+				"VALUES ('Hello World', 'Test article #1', '2011-06-08 12:10:00'), "+
+				"('Second title', 'Test article #2', '2011-07-15 09:34:39'), "+
+				"('Third title', 'Test article #3', '2011-09-07 20:00:00')"),
+			mytestcase.sql.query('DELETE FROM '+mytestcase.table+' WHERE id=:id LIMIT 1')
+		);
+		test.ok(del, "Failed to create callback: del");
+		test.strictEqual(typeof del, 'function', "Failed to create callback: del");
+		var select = mytestcase.sql.query('SELECT * FROM '+mytestcase.table);
+		test.ok(select, "Failed to create callback: select");
+		test.strictEqual(typeof select, 'function', "Failed to create callback: select");
+		del({'id':2});
+		setTimeout(function() { // DO NOT USE CODE LIKE THIS IN REAL SOFTWARE :-)
+			select(function(err, state) {
+				test.ok(!err, "Error: " + err);
+				test.ok(state);
+				test.ok(state._rows);
+				test.ok(state._results);
+				test.strictEqual( state._rows.length, 2);
+				test.strictEqual( state._results.length, 1);
+				test.strictEqual( state._results[0].length, 2);
+				test.strictEqual( sys.inspect(state._rows[0]), sys.inspect(state._results[0][0]), "Article #1 failed");
+				test.strictEqual( sys.inspect(state._rows[1]), sys.inspect(state._results[0][1]), "Article #2 failed");
+				
+				test.strictEqual( state._rows[0].id, 1);
+				test.strictEqual( state._rows[0].title, 'Hello World');
+				test.strictEqual( state._rows[0].text, 'Test article #1');
+				test.strictEqual( sys.inspect(state._rows[0].created), 'Wed, 08 Jun 2011 09:10:00 GMT');
+				
+				test.strictEqual( state._rows[1].id, 3);
+				test.strictEqual( state._rows[1].title, 'Third title');
+				test.strictEqual( state._rows[1].text, 'Test article #3');
+				test.strictEqual( sys.inspect(state._rows[1].created), 'Wed, 07 Sep 2011 17:00:00 GMT');
+				
+				test.done();
+			});
+		}, 250);
+	},
+	/* Test for DELETE without selection and no user defined callback */
+	query_delete_without_options_callback: function(test){
+		var mytestcase = this;
+		test.expect(21);
+		var del = mytestcase.sql.group(
+			mytestcase.sql.query('INSERT INTO '+mytestcase.table+" (title, text, created) "+
+				"VALUES ('Hello World', 'Test article #1', '2011-06-08 12:10:00'), "+
+				"('Second title', 'Test article #2', '2011-07-15 09:34:39'), "+
+				"('Third title', 'Test article #3', '2011-09-07 20:00:00')"),
+			mytestcase.sql.query('DELETE FROM '+mytestcase.table+' WHERE id = 2 LIMIT 1')
+		);
+		test.ok(del, "Failed to create callback: del");
+		test.strictEqual(typeof del, 'function', "Failed to create callback: del");
+		var select = mytestcase.sql.query('SELECT * FROM '+mytestcase.table);
+		test.ok(select, "Failed to create callback: select");
+		test.strictEqual(typeof select, 'function', "Failed to create callback: select");
+		del();
+		setTimeout(function() { // DO NOT USE CODE LIKE THIS IN REAL SOFTWARE :-)
+			select(function(err, state) {
+				test.ok(!err, "Error: " + err);
+				test.ok(state);
+				test.ok(state._rows);
+				test.ok(state._results);
+				test.strictEqual( state._rows.length, 2);
+				test.strictEqual( state._results.length, 1);
+				test.strictEqual( state._results[0].length, 2);
+				test.strictEqual( sys.inspect(state._rows[0]), sys.inspect(state._results[0][0]), "Article #1 failed");
+				test.strictEqual( sys.inspect(state._rows[1]), sys.inspect(state._results[0][1]), "Article #2 failed");
+				
+				test.strictEqual( state._rows[0].id, 1);
+				test.strictEqual( state._rows[0].title, 'Hello World');
+				test.strictEqual( state._rows[0].text, 'Test article #1');
+				test.strictEqual( sys.inspect(state._rows[0].created), 'Wed, 08 Jun 2011 09:10:00 GMT');
+				
+				test.strictEqual( state._rows[1].id, 3);
+				test.strictEqual( state._rows[1].title, 'Third title');
+				test.strictEqual( state._rows[1].text, 'Test article #3');
+				test.strictEqual( sys.inspect(state._rows[1].created), 'Wed, 07 Sep 2011 17:00:00 GMT');
+				
+				test.done();
+			});
+		}, 250);
 	}
 });
 
--- test/test-pg.js
+++ test/test-pg.js
@@ -28,6 +28,7 @@ module.exports = testCase({
 			callback(err);
 		});
 	},
+	/* Test MySQL backend API */
 	backend: function(test){
 		var mytestcase = this;
 		var backend = mytestcase.sql.backend;
@@ -40,7 +41,8 @@ module.exports = testCase({
 		test.strictEqual(typeof backend.disconnect, 'function', "Missing .disconnect");
 		test.done();
 	},
-	query_insert: function(test){
+	/* Test INSERT with values and callback function */
+	query_insert_with_options_callback: function(test){
 		var mytestcase = this;
 		test.expect(4);
 		var cb = mytestcase.sql.query('INSERT INTO '+mytestcase.table+' (title, text, created) VALUES (:title, :text, :created)');
@@ -53,7 +55,8 @@ module.exports = testCase({
 			test.done();
 		});
 	},
-	query_select: function(test){
+	/* Test SELECT with empty values and callback function */
+	query_select_with_emptyoptions_callback: function(test){
 		var mytestcase = this;
 		test.expect(24);
 		var cb = mytestcase.sql.group(
@@ -95,7 +98,8 @@ module.exports = testCase({
 			test.done();
 		});
 	},
-	query_select_single: function(test){
+	/* Test SELECT+LIMIT with selection and callback function */
+	query_select_single_with_options_callback: function(test){
 		var mytestcase = this;
 		test.expect(20);
 		var cb = mytestcase.sql.group(
@@ -132,7 +136,8 @@ module.exports = testCase({
 			test.done();
 		});
 	},
-	query_delete: function(test){
+	/* Test DELETE with selection and callback function */
+	query_delete_with_options_callback: function(test){
 		var mytestcase = this;
 		test.expect(19);
 		var cb = mytestcase.sql.group(
@@ -169,7 +174,8 @@ module.exports = testCase({
 			test.done();
 		});
 	},
-	query_update: function(test){
+	/* Test UPDATE with values and callback function */
+	query_update_with_options_callback: function(test){
 		var mytestcase = this;
 		test.expect(24);
 		var cb = mytestcase.sql.group(
@@ -208,6 +214,87 @@ module.exports = testCase({
 			test.strictEqual( state._rows[2].title, 'Third title');
 			test.strictEqual( state._rows[2].text, 'Test article #3');
 			test.strictEqual( sys.inspect(state._rows[2].created), 'Wed, 07 Sep 2011 17:00:00 GMT');
+			
+			test.done();
+		});
+	},
+	/* Test SELECT without values and with callback function */
+	query_select_without_options_with_callback: function(test){
+		var mytestcase = this;
+		test.expect(24);
+		var cb = mytestcase.sql.group(
+			mytestcase.sql.query('INSERT INTO '+mytestcase.table+" (title, text, created) "+
+				"VALUES ('Hello World', 'Test article #1', '2011-06-08 12:10:00'), "+
+				"('Second title', 'Test article #2', '2011-07-15 09:34:39'), "+
+				"('Third title', 'Test article #3', '2011-09-07 20:00:00')"),
+			mytestcase.sql.query('SELECT * FROM '+mytestcase.table + ' ORDER BY id')
+		);
+		test.ok(cb, "Failed to create callback");
+		test.strictEqual(typeof cb, 'function', "Failed to create callback");
+		cb(function(err, state) {
+			test.ok(!err, "Error: " + err);
+			test.ok(state, "state");
+			test.ok(state._rows, "state._rows");
+			test.ok(state._results, "state._results");
+			test.strictEqual( state._rows.length, 3, "state._rows.length, 3");
+			test.strictEqual( state._results.length, 1, "state._results.length, 1");
+			test.strictEqual( state._results[0].length, 3, "state._results[0].length, 3");
+			test.strictEqual( sys.inspect(state._rows[0]), sys.inspect(state._results[0][0]), "Article #1 failed");
+			test.strictEqual( sys.inspect(state._rows[1]), sys.inspect(state._results[0][1]), "Article #2 failed");
+			test.strictEqual( sys.inspect(state._rows[2]), sys.inspect(state._results[0][2]), "Article #3 failed");
+			
+			test.strictEqual( state._rows[0].id, 1, "state._rows[0].id, 1");
+			test.strictEqual( state._rows[0].title, 'Hello World');
+			test.strictEqual( state._rows[0].text, 'Test article #1');
+			test.strictEqual( sys.inspect(state._rows[0].created), 'Wed, 08 Jun 2011 09:10:00 GMT');
+			
+			test.strictEqual( state._rows[1].id, 2, "state._rows[1].id, 2");
+			test.strictEqual( state._rows[1].title, 'Second title');
+			test.strictEqual( state._rows[1].text, 'Test article #2');
+			test.strictEqual( sys.inspect(state._rows[1].created), 'Fri, 15 Jul 2011 06:34:39 GMT');
+			
+			test.strictEqual( state._rows[2].id, 3, "state._rows[2].id, 3");
+			test.strictEqual( state._rows[2].title, 'Third title');
+			test.strictEqual( state._rows[2].text, 'Test article #3');
+			test.strictEqual( sys.inspect(state._rows[2].created), 'Wed, 07 Sep 2011 17:00:00 GMT');
+			
+			test.done();
+		});
+	},
+	/* Test SELECT with values and with callback function */
+	query_select_single_without_options_callback: function(test){
+		var mytestcase = this;
+		test.expect(20);
+		var cb = mytestcase.sql.group(
+			mytestcase.sql.query('INSERT INTO '+mytestcase.table+" (title, text, created) "+
+				"VALUES ('Hello World', 'Test article #1', '2011-06-08 12:10:00'), "+
+				"('Second title', 'Test article #2', '2011-07-15 09:34:39'), "+
+				"('Third title', 'Test article #3', '2011-09-07 20:00:00')"),
+			mytestcase.sql.query('SELECT * FROM '+mytestcase.table + ' WHERE id=1 LIMIT 1')
+		);
+		test.ok(cb, "Failed to create callback");
+		test.strictEqual(typeof cb, 'function', "Failed to create callback");
+		cb(function(err, state) {
+			test.ok(!err, "Error: " + err);
+			test.ok(state);
+			test.ok(state._rows);
+			test.ok(state._results);
+			test.strictEqual( state._rows.length, 1);
+			test.strictEqual( state._results.length, 1);
+			test.strictEqual( state._results[0].length, 1);
+			test.strictEqual( sys.inspect(state._rows[0]), sys.inspect(state._results[0][0]), "Article #1 failed");
+			test.strictEqual( sys.inspect(state._rows[1]), sys.inspect(state._results[0][1]), "Article #2 failed");
+			test.strictEqual( sys.inspect(state._rows[2]), sys.inspect(state._results[0][2]), "Article #3 failed");
+			
+			test.strictEqual( state._rows[0].id, 1);
+			test.strictEqual( state._rows[0].title, 'Hello World');
+			test.strictEqual( state._rows[0].text, 'Test article #1');
+			test.strictEqual( sys.inspect(state._rows[0].created), 'Wed, 08 Jun 2011 09:10:00 GMT');
+			
+			test.strictEqual( state.id, 1);
+			test.strictEqual( state.title, 'Hello World');
+			test.strictEqual( state.text, 'Test article #1');
+			test.strictEqual( sys.inspect(state.created), 'Wed, 08 Jun 2011 09:10:00 GMT');
 			
 			test.done();
 		});
