482570078a43584116b25d927bf626e1430dd4dd
Jaakko-Heikki Heusala <jheusala@iki.fi>
2011-09-09 17:00:37 +0300

    Added some unit tests

--- test/test-mysql.js
+++ test/test-mysql.js
@@ -53,49 +53,6 @@ module.exports = testCase({
 			test.done();
 		});
 	},
-	/* Test for SELECT with empty options and user defined callback function */
-	query_select_with_empty_options_callback: function(test){
-		var mytestcase = this;
-		test.expect(24);
-		var cb = mytestcase.sql.group(
-			mytestcase.sql.query('INSERT INTO '+mytestcase.table+" (title, text, created) "+
-				"VALUES ('Hello World', 'Test article #1', '2011-06-08 12:10:00'), "+
-				"('Second title', 'Test article #2', '2011-07-15 09:34:39'), "+
-				"('Third title', 'Test article #3', '2011-09-07 20:00:00')"),
-			mytestcase.sql.query('SELECT * FROM '+mytestcase.table)
-		);
-		test.ok(cb, "Failed to create callback");
-		test.strictEqual(typeof cb, 'function', "Failed to create callback");
-		cb({}, function(err, state) {
-			test.ok(!err, "Error: " + err);
-			test.ok(state);
-			test.ok(state._rows);
-			test.ok(state._results);
-			test.strictEqual( state._rows.length, 3);
-			test.strictEqual( state._results.length, 1);
-			test.strictEqual( state._results[0].length, 3);
-			test.strictEqual( sys.inspect(state._rows[0]), sys.inspect(state._results[0][0]), "Article #1 failed");
-			test.strictEqual( sys.inspect(state._rows[1]), sys.inspect(state._results[0][1]), "Article #2 failed");
-			test.strictEqual( sys.inspect(state._rows[2]), sys.inspect(state._results[0][2]), "Article #3 failed");
-			
-			test.strictEqual( state._rows[0].id, 1);
-			test.strictEqual( state._rows[0].title, 'Hello World');
-			test.strictEqual( state._rows[0].text, 'Test article #1');
-			test.strictEqual( sys.inspect(state._rows[0].created), 'Wed, 08 Jun 2011 09:10:00 GMT');
-			
-			test.strictEqual( state._rows[1].id, 2);
-			test.strictEqual( state._rows[1].title, 'Second title');
-			test.strictEqual( state._rows[1].text, 'Test article #2');
-			test.strictEqual( sys.inspect(state._rows[1].created), 'Fri, 15 Jul 2011 06:34:39 GMT');
-			
-			test.strictEqual( state._rows[2].id, 3);
-			test.strictEqual( state._rows[2].title, 'Third title');
-			test.strictEqual( state._rows[2].text, 'Test article #3');
-			test.strictEqual( sys.inspect(state._rows[2].created), 'Wed, 07 Sep 2011 17:00:00 GMT');
-			
-			test.done();
-		});
-	},
 	/* Test for SELECT without options and with user defined callback function */
 	query_select_without_options_with_callback: function(test){
 		var mytestcase = this;
@@ -371,7 +328,84 @@ module.exports = testCase({
 			test.strictEqual(state._insertId, 1, "state._insertId failed");
 			test.done();
 		});
+	},
+	/* Test for transactions */
+	transaction_successfull: function(test){
+		var mytestcase = this;
+		test.expect(24);
+		var cb = mytestcase.sql.group(
+			mytestcase.sql.begin(),
+			mytestcase.sql.query('INSERT INTO '+mytestcase.table+" (title, text, created) "+
+				"VALUES ('Hello World', 'Test article #1', '2011-06-08 12:10:00'), "+
+				"('Second title', 'Test article #2', '2011-07-15 09:34:39'), "+
+				"('Third title', 'Test article #3', '2011-09-07 20:00:00')"),
+			mytestcase.sql.commit(),
+			mytestcase.sql.query('SELECT * FROM '+mytestcase.table)
+		);
+		test.ok(cb, "Failed to create callback");
+		test.strictEqual(typeof cb, 'function', "Failed to create callback");
+		cb({}, function(err, state) {
+			test.ok(!err, "Error: " + err);
+			test.ok(state);
+			test.ok(state._rows);
+			test.ok(state._results);
+			test.strictEqual( state._rows.length, 3);
+			test.strictEqual( state._results.length, 1);
+			test.strictEqual( state._results[0].length, 3);
+			test.strictEqual( sys.inspect(state._rows[0]), sys.inspect(state._results[0][0]), "Article #1 failed");
+			test.strictEqual( sys.inspect(state._rows[1]), sys.inspect(state._results[0][1]), "Article #2 failed");
+			test.strictEqual( sys.inspect(state._rows[2]), sys.inspect(state._results[0][2]), "Article #3 failed");
+			
+			test.strictEqual( state._rows[0].id, 1);
+			test.strictEqual( state._rows[0].title, 'Hello World');
+			test.strictEqual( state._rows[0].text, 'Test article #1');
+			test.strictEqual( sys.inspect(state._rows[0].created), 'Wed, 08 Jun 2011 09:10:00 GMT');
+			
+			test.strictEqual( state._rows[1].id, 2);
+			test.strictEqual( state._rows[1].title, 'Second title');
+			test.strictEqual( state._rows[1].text, 'Test article #2');
+			test.strictEqual( sys.inspect(state._rows[1].created), 'Fri, 15 Jul 2011 06:34:39 GMT');
+			
+			test.strictEqual( state._rows[2].id, 3);
+			test.strictEqual( state._rows[2].title, 'Third title');
+			test.strictEqual( state._rows[2].text, 'Test article #3');
+			test.strictEqual( sys.inspect(state._rows[2].created), 'Wed, 07 Sep 2011 17:00:00 GMT');
+			
+			test.done();
+		});
 	}
+	/* Test for transactions */
+	/* MySQL does not support transactions... really? MySQL, really?
+	transaction_failing: function(test){
+		var mytestcase = this;
+		test.expect(12);
+		var cb = mytestcase.sql.group(
+			mytestcase.sql.begin(),
+			mytestcase.sql.query('INSERT INTO '+mytestcase.table+" (title, text, created) "+
+				"VALUES ('Hello World', 'Test article #1', '2011-06-08 12:10:00'), "+
+				"('Second title', 'Test article #2', '2011-07-15 09:34:39'), "+
+				"('Third title', 'Test article #3', '2011-09-07 20:00:00')"),
+			mytestcase.sql.rollback(),
+			mytestcase.sql.query('SELECT * FROM '+mytestcase.table)
+		);
+		test.ok(cb, "Failed to create callback");
+		test.strictEqual(typeof cb, 'function', "Failed to create callback");
+		cb({}, function(err, state) {
+			test.ok(!err, "Error: " + err);
+			test.ok(state);
+			test.ok(state._rows);
+			test.ok(state._results);
+			test.strictEqual( state._rows.length, 0);
+			test.strictEqual( state._results.length, 1);
+			test.strictEqual( state._results[0].length, 0);
+			test.strictEqual( state._rows[0], undefined);
+			test.strictEqual( state._rows[1], undefined);
+			test.strictEqual( state._rows[2], undefined);
+			
+			test.done();
+		});
+	}
+	*/
 });
 
 /* EOF */
